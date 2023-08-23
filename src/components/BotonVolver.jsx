2688d277552983f1b6cfabe4ca56f9e90645b156
DAPS2103 <daps2103@gmail.com>
2023-08-23 17:52:20 -0400

    add new condition img

--- src/components/BotonVolver.jsx
+++ src/components/BotonVolver.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import imagen from "../assets/img/volver.png";
 
 export function BotonVolver({ direccion }) {
   const navegar = useNavigate();
+  const [error, setError] = useState(false);
 
   return (
     <div>
       <img
         className="w-12 h-12 mt-3 ml-3 absolute left-0 top-0 cursor-pointer"
-        src="https://cdn-icons-png.flaticon.com/128/4495/4495664.png"
+        src={
+          error
+            ? imagen
+            : "https://cdn-icons-png.flaticon.com/128/4495/4495664.png"
+        }
         alt="Volver"
+        onError={() => setError(true)}
         onClick={() => navegar(direccion)}
       />
     </div>
   );
 }
+
+// https://cdn-icons-png.flaticon.com/128/4495/4495664.png
