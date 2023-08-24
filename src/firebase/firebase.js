061388d7b1db370706d5d18f9e645e4c1f011ae7
DAPS2103 <daps2103@gmail.com>
2023-08-24 18:03:46 -0400

    deleted old comments

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -26,4 +25,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-//export const analytics = getAnalytics(app);
