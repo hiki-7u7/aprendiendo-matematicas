abbbaab1d58ce6a5d1f8f08595ad9be47730b558
DAPS2103 <daps2103@gmail.com>
2023-08-24 18:33:59 -0400

    add get student and get rut profesor

--- src/pages/JoinStudent.jsx
+++ src/pages/JoinStudent.jsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs, where, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  addDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import { BotonVolver } from "../components/BotonVolver";
 import { Alumnos } from "../components/Alumnos";
 import { Alert } from "../components/Alert";
+import { useAuth } from "../context/authContext";
 
 export function JoinStudent() {
   const navegar = useNavigate();
 
   // codigo para obtener el nombre y rut del alumno
+  const { user } = useAuth();
   const [alumnos, setAlumnos] = useState([]);
   const [rut, setRut] = useState("");
   const [error, setError] = useState(false);
+
+  // codigo para obtener el ID del profesor
+  const [idProfesor, setIdProfesor] = useState("");
+  const obtenerIdProfesor = async () => {
+    const q = query(
+      collection(db, "usuarios"),
+      where("email", "==", user.email)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      setIdProfesor(doc.data().rut);
+    });
+  };
+
+  // codigo para obtener el ID del profesor
+  useEffect(() => {
+    obtenerIdProfesor();
+  }, []);
 
   const obtenerAlumno = async () => {
     if (rut) {
@@ -26,11 +55,6 @@ export function JoinStudent() {
 
       const listaAlumnos = querySnapshot.docs.map((doc) => doc.data());
 
-      console.log(
-        "Estado actual de alumnos:",
-        alumnos.map((a) => a.nombre)
-      );
-
       const alumnoEncontrado = listaAlumnos.find(
         (alumno) => alumno.rut === rut
       );
@@ -42,15 +66,52 @@ export function JoinStudent() {
         setError("El alumno ya ha sido agregado");
       } else {
         setError("");
-        setAlumnos([...alumnos, alumnoEncontrado]);
+
+        const EstudianteConIdProfesor = {
+          ...alumnoEncontrado,
+          ProfesorAsignado: idProfesor,
+        };
+        try {
+          const docRef = await addDoc(
+            collection(db, "usuarios"),
+            EstudianteConIdProfesor
+          );
+          setAlumnos([...alumnos, EstudianteConIdProfesor]);
+          //console.log("Document written with ID: ", idProfesor);
+          //console.log(docRef);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
       }
     }
   };
 
-  console.log(
+  const registrarProfesorAsignado = async () => {
+    const alumnosConIdProfesor = alumnos.map((alumno) => ({
+      ...alumno,
+      ProfesorAsignado: idProfesor,
+    }));
+    setAlumnos(alumnosConIdProfesor);
+    console.log("Alumnos con id profesor:", alumnosConIdProfesor);
+
+    for (const alumno of alumnosConIdProfesor) {
+      try {
+        const docRef = doc(db, "usuarios", alumno.rut);
+        await updateDoc(docRef, {
+          ProfesorAsignado: alumno.ProfesorAsignado,
+        });
+        console.log("Documento Actualizado");
+      } catch (e) {
+        console.error("Error en Actualizacion de Documento: ", e);
+      }
+    }
+  };
+
+  /*console.log(
     "Estado actual de alumnos:",
     alumnos.map((a) => a.nombre)
-  );
+  );*/
+  //console.log("ID del profesor:", idProfesor);
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto ">
@@ -92,7 +153,10 @@ export function JoinStudent() {
         <Alert message={error} /> // Mostrar la alerta
       )}
 
-      <button className="bg-orange-500 hover:bg-orange-300  rounded-full px-2 focus:outline-none focus:shadow-outline">
+      <button
+        onClick={registrarProfesorAsignado}
+        className="bg-orange-500 hover:bg-orange-300  rounded-full px-2 focus:outline-none focus:shadow-outline"
+      >
         Registrar
       </button>
 
