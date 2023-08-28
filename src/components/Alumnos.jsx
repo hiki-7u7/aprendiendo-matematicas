6b40bd9cf2d9ca4a30b26870a7a26182462e133e
DAPS2103 <daps2103@gmail.com>
2023-08-28 18:41:18 -0400

    add function deleted alumno

--- src/components/Alumnos.jsx
+++ src/components/Alumnos.jsx
@@ -1,22 +1,28 @@
-import { useState, useEffect } from "react";
-import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
-import { collection, getDocs, where, query } from "firebase/firestore";
-import { db } from "../firebase/firebase";
-import { useLocation } from "react-router-dom";
-
 export function Alumnos(props) {
-  console.log("Props recibidos");
+  const eliminarAlumno = async () => {
+    try {
+      // Eliminar alumno de la base de datos
+      //await deleteDoc(doc(db, "Profesor", props.rut));
+      //console.log("Alumno eliminado");
+
+      // Actualizar lista de alumnos
+      props.actualizarListaAlumnos(props.rut);
+    } catch (error) {
+      console.log(" Error al eliminar el alumno: ", error);
+    }
+  };
+
   return (
-    <div className="flex justify-center mt-10">
-      <form className="flex ">
-        <p>
+    <div className=" justify-center mt-10 ">
+      <form className="flex  items-center">
+        <p className="border-2 border-black rounded-2xl  p-1 m-1">
           {props.nombre} {props.apellido} - {props.rut}
         </p>
         <img
-          className="h-5 w-5 ml-2 cursor-pointer mt-1"
+          className="h-5 w-5 ml-2 cursor-pointer bg-red-500 rounded-full hover:bg-red-300 "
           src="https://cdn-icons-png.flaticon.com/128/1617/1617543.png"
           alt="Borrar alumno"
+          onClick={eliminarAlumno}
         />
       </form>
     </div>
