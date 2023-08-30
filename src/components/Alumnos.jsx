d73c579581f59eabcafa33d05da1b49c3772c730
DAPS2103 <daps2103@gmail.com>
2023-08-30 16:38:25 -0400

    update function deleted listAlum, and teacher

--- src/components/Alumnos.jsx
+++ src/components/Alumnos.jsx
@@ -1,12 +1,65 @@
+import {
+  updateDoc,
+  doc,
+  collection,
+  where,
+  query,
+  getDocs,
+  arrayRemove,
+} from "firebase/firestore";
+import { db } from "../firebase/firebase.js";
+
 export function Alumnos(props) {
   const eliminarAlumno = async () => {
     try {
-      // Eliminar alumno de la base de datos
-      //await deleteDoc(doc(db, "Profesor", props.rut));
-      //console.log("Alumno eliminado");
+      // Eliminar alumno
+      const profesorQuery = query(
+        collection(db, "Profesor"),
+        where("rut", "==", props.profesorAsignado)
+      );
+      console.log("rut de profespr", props.profesorAsignado);
 
-      // Actualizar lista de alumnos
-      props.actualizarListaAlumnos(props.rut);
+      // codigo para obtener datos
+      const querySnapshotProfesor = await getDocs(profesorQuery);
+
+      if (!querySnapshotProfesor.empty) {
+        // Si se encuentra el profesor, se elimina
+        const profesor = querySnapshotProfesor.docs[0];
+        const profesorRef = doc(db, "Profesor", profesor.id);
+
+        console.log("profesor", profesor.id);
+        // actualizar datos
+        await updateDoc(profesorRef, {
+          alumnos: arrayRemove(props.rut),
+        });
+      } else {
+        console.log("No se encontro el profesor");
+      }
+
+      // Eliminar profesor asignado
+      const estudianteQuery = query(
+        collection(db, "Estudiante"),
+        where("rut", "==", props.rut)
+      );
+
+      // codigo para obtener datos
+      const querySnapshot = await getDocs(estudianteQuery);
+
+      if (!querySnapshot.empty) {
+        // Si se encuentra el alumno, se elimina
+        const estudiante = querySnapshot.docs[0];
+        const estudianteRef = doc(db, "Estudiante", estudiante.id);
+
+        // actualizar datos
+        await updateDoc(estudianteRef, {
+          ProfesorAsignado: "",
+        });
+
+        // Actualizar lista de alumnos
+        props.actualizarListaAlumnos(props.rut);
+      } else {
+        console.log("No se encontro el alumno");
+      }
     } catch (error) {
       console.log(" Error al eliminar el alumno: ", error);
     }
