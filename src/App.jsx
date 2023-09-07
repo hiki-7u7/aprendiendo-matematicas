6c959fdaa1eae372183520140c2752f946bbce54
DAPS2103 <daps2103@gmail.com>
2023-09-07 16:30:56 -0300

    change Register to Registro

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Alumno } from "./pages/Alumno.jsx";
 import { Login } from "./pages/Login.jsx";
-import { Register } from "./pages/Register.jsx";
+import { Registro } from "./pages/Registro.jsx";
 import { AuthProvider } from "./context/authContext.jsx";
 import { RutaProtegida } from "./components/ProtectedRoute.jsx";
 import { SelectRole } from "./pages/SelectRole.jsx";
 import { ResetPassword } from "./pages/ResetPassword.jsx";
-import { Register_2 } from "./pages/Register_2.jsx";
 import { JoinStudent } from "./pages/JoinStudent.jsx";
 import { Profesor } from "./pages/Profesor.jsx";
 
@@ -27,8 +26,7 @@ function App() {
           />
           <Route path="/Login" element={<Login />} />
           <Route path="/SelectRole" element={<SelectRole />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Register_2" element={<Register_2 />} />
+          <Route path="/Registro" element={<Registro />} />
           <Route path="/ResetPassword" element={<ResetPassword />} />
           <Route
             path="/JoinStudent"
