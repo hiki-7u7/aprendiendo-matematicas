4b761df39fa09ac1c465efaf9cc2196a0a24c251
DAPS2103 <daps2103@gmail.com>
2023-09-11 12:53:00 -0300

    create SobrePagina

---
+++ src/pages/SobrePagina.jsx
@@ -0,0 +1,8 @@
+export function SobrePagina() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen ">
+      <h1>Sobre Pagina</h1>
+      <p>Esta es la pagina sobre nosotros</p>
+    </div>
+  );
+}
