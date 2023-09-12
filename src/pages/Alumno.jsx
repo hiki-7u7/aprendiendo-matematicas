eb9d918c13173635307e4963bffdf313bb9504b9
DAPS2103 <daps2103@gmail.com>
2023-09-12 15:58:23 -0300

    modif. pagina Alumno, con footer y header

--- src/pages/Alumno.jsx
+++ src/pages/Alumno.jsx
@@ -1,61 +1,47 @@
-// Objetivo: componente Home
-import { useAuth } from "../context/authContext.jsx";
-import { collection, getDocs, where, query } from "firebase/firestore";
-import { db } from "../firebase/firebase.js";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Cabecera } from "../components/Cabecera.jsx";
+import { PieDePagina } from "../components/PieDePagina.jsx";
 
-//funcion que exporta el componente Home
+const unidades = [
+  { id: 1, nombre: "Unidad 1: Números y operaciones" },
+  { id: 2, nombre: "Unidad 2: Patrones y álgebra" },
+  { id: 3, nombre: "Unidad 3: Geometría" },
+  { id: 4, nombre: "Unidad 4: Medición" },
+  { id: 5, nombre: "Unidad 5: Datos y probabilidades" },
+];
+
 export function Alumno() {
-  //funcion que permite manejar el contexto de autenticacion
-  const { user, CerrarSesion, cargando } = useAuth();
-  const [usuarioRol, setUsuarioRol] = useState();
-  const [usuarioNombre, setUsuarioNombre] = useState();
-  const [usuarioApellido, setUsuarioApellido] = useState();
-  const [usuarioRut, setUsuarioRut] = useState();
-
-  //funcion que permite obtener los datos de la base de datos
-  const obtenerDatos = async () => {
-    const q = query(
-      collection(db, "Estudiante"),
-      where("email", "==", user.email)
-    );
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      setUsuarioRol(doc.data().rol);
-      setUsuarioNombre(doc.data().nombre);
-      setUsuarioApellido(doc.data().apellido);
-      setUsuarioRut(doc.data().rut);
-    });
-  };
-
-  //funcion que permite obtener los datos de la base de datos
-  useEffect(() => {
-    obtenerDatos();
-  }, []);
-
-  // funcion que permite validar si el usuario esta logueado
-  const handlelogout = async () => {
-    await CerrarSesion();
-  };
-  if (cargando) return <p>Cargando...</p>;
-
-  //retorno del componente
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen ">
+    <div className="flex flex-col min-h-screen">
       <Cabecera />
-      <h1>Alumno</h1>
-      <p>Bienvenido {usuarioNombre}</p>
-      <p>Usted es: {usuarioRol}</p>
-      <p>Apellido: {usuarioApellido}</p>
-      <p>Rut: {usuarioRut}</p>
-      <button
-        className="bg-orange-500 hover:bg-orange-300  rounded-full px-2 focus:outline-none focus:shadow-outline"
-        onClick={handlelogout}
-      >
-        Cerrar Sesion
-      </button>
+
+      <div className="flex-1 flex items-center justify-center">
+        <div className="max-w-screen-xl mx-auto">
+          <h1 className="text-3xl font-bold mt-16  text-center">
+            Unidades del curso
+          </h1>
+
+          <div className="grid grid-rows-1 sm:grid-rows-2 md:grid-rows-3 lg:grid-rows-4 gap-4 mt-4 p-4">
+            {unidades.map((unidad) => (
+              <div
+                key={unidad.id}
+                className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 text-center"
+              >
+                <h2 className="text-2xl font-semibold">{unidad.nombre}</h2>
+                <Link
+                  to={`/unidad/${unidad.id}`}
+                  className="text-blue-500 hover:underline mt-4 block text-xl"
+                >
+                  Ver unidad
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <PieDePagina />
     </div>
   );
 }
