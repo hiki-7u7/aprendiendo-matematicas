ecf3c68a6bfa1f504a342cbc3443b87c9a0407ec
DAPS2103 <daps2103@gmail.com>
2023-09-12 15:46:37 -0300

    crear PieDePagina

---
+++ src/components/PieDePagina.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function PieDePagina() {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center flex justify-center items-end h-full">
+        <p>
+          &copy; 2023 Diego Andres Peña Soto - Todos los derechos reservados
+        </p>
+      </div>
+    </footer>
+  );
+}
