cb7a239dda024e36a67607291c3a81334c3eca83
DAPS2103 <daps2103@gmail.com>
2023-09-28 12:21:49 -0300

    modif app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Alumno } from "./pages/Alumno.jsx";
 import { Login } from "./pages/Login.jsx";
@@ -10,6 +9,13 @@ import { ResetPassword } from "./pages/ResetPassword.jsx";
 import { JoinStudent } from "./pages/JoinStudent.jsx";
 import { Profesor } from "./pages/Profesor.jsx";
 import { SobrePagina } from "./pages/SobrePagina.jsx";
+import { Unidad_1 } from "./pages/Unidad_1.jsx";
+import { Unidad_2 } from "./pages/Unidad_2.jsx";
+import { Unidad_3 } from "./pages/Unidad_3.jsx";
+import { Unidad_4 } from "./pages/Unidad_4.jsx";
+import { Unidad_5 } from "./pages/Unidad_5.jsx";
+import { Ejercicio_1_1 } from "./pages/Ejercicio_1_1.jsx";
+import { Perfil } from "./pages/Perfil.jsx";
 
 //funcion que exporta el componente App
 function App() {
@@ -45,7 +51,64 @@ function App() {
               </RutaProtegida>
             }
           />
+          <Route
+            path="/perfil"
+            element={
+              <RutaProtegida>
+                <Perfil />
+              </RutaProtegida>
+            }
+          />
+
           <Route path="/SobrePagina" element={<SobrePagina />} />
+          <Route
+            path="/unidad/1"
+            element={
+              <RutaProtegida>
+                <Unidad_1 />
+              </RutaProtegida>
+            }
+          />
+          <Route
+            path="/unidad/2"
+            element={
+              <RutaProtegida>
+                <Unidad_2 />
+              </RutaProtegida>
+            }
+          />
+          <Route
+            path="/unidad/3"
+            element={
+              <RutaProtegida>
+                <Unidad_3 />
+              </RutaProtegida>
+            }
+          />
+          <Route
+            path="/unidad/4"
+            element={
+              <RutaProtegida>
+                <Unidad_4 />
+              </RutaProtegida>
+            }
+          />
+          <Route
+            path="/unidad/5"
+            element={
+              <RutaProtegida>
+                <Unidad_5 />
+              </RutaProtegida>
+            }
+          />
+          <Route
+            path="/unidad/1/ejercicio 1"
+            element={
+              <RutaProtegida>
+                <Ejercicio_1_1 />
+              </RutaProtegida>
+            }
+          />
         </Routes>
       </AuthProvider>
     </div>
