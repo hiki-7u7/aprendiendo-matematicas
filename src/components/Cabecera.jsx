84d056f5cb890a1743696b1648b37db4d11df6d4
DAPS2103 <daps2103@gmail.com>
2023-09-28 12:25:28 -0300

    modif. cabecera

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa"; // Importa el icono de usuario de FontAwesome
+import { useAuth } from "../context/authContext.jsx"; // Importar el contexto de autenticación
 
 export function Cabecera() {
+  const { CerrarSesion } = useAuth(); // Importar función para cerrar sesión
   const [menuVisible, setMenuVisible] = useState(false);
   const [menuUnidadesVisible, setMenuUnidadesVisible] = useState(false);
+  const [perfilMenuVisible, setPerfilMenuVisible] = useState(false); // Agregar estado para el menú de perfil
 
   const unidades = [
-    { id: 1, nombre: "Unidad 1" },
-    { id: 2, nombre: "Unidad 2" },
-    { id: 3, nombre: "Unidad 3" },
-    { id: 4, nombre: "Unidad 4" },
-    { id: 5, nombre: "Unidad 5" },
-    { id: 6, nombre: "Unidad 6" },
+    { id: 1, nombre: "Unidad 1: Números y operaciones" },
+    { id: 2, nombre: "Unidad 2: Patrones y álgebra" },
+    { id: 3, nombre: "Unidad 3: Geometría" },
+    { id: 4, nombre: "Unidad 4: Medición" },
+    { id: 5, nombre: "Unidad 5: Datos y probabilidades" },
   ];
 
   // Funciones para mostrar y ocultar el menú principal
@@ -25,6 +27,18 @@ export function Cabecera() {
   const handleUnidadesClick = () => {
     setMenuUnidadesVisible(!menuUnidadesVisible);
     setMenuVisible(true); // Asegurarse de que el menú principal esté oculto
+  };
+
+  // Funciones para mostrar y ocultar el menú de perfil
+  const handlePerfilClick = () => {
+    setPerfilMenuVisible(!perfilMenuVisible);
+    setMenuVisible(true); // Asegurarse de que el menú principal esté oculto
+    setMenuUnidadesVisible(false); // Asegurarse de que el menú de unidades esté oculto
+  };
+
+  // Función para cerrar sesión
+  const handlelogout = async () => {
+    await CerrarSesion();
   };
 
   return (
@@ -71,12 +85,34 @@ export function Cabecera() {
             </div>
           )}
         </div>
-        <Link
-          to="/perfil"
-          className="bg-green-500 hover:bg-green-300 px-4 py-2 rounded-full transition duration-300"
-        >
-          <FaUser /> {/* Icono de usuario */}
-        </Link>
+        {/* Menú de perfil */}
+        <div className="relative inline-block text-left">
+          <button
+            onClick={handlePerfilClick}
+            className="bg-green-500 hover:bg-green-300 px-4 py-2 rounded-full transition duration-1000"
+          >
+            <FaUser /> {/* Icono de usuario */}
+          </button>
+          {perfilMenuVisible && (
+            <div className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+              <div className="py-1">
+                <Link
+                  to="/perfil"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Ir a Perfil
+                </Link>
+
+                <Link
+                  onClick={handlelogout}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Cerrar Sesión
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
       </nav>
       {/* Mostrar en pantallas pequeñas */}
       <div className="md:hidden">
@@ -125,12 +161,33 @@ export function Cabecera() {
                   </div>
                 )}
               </div>
-              <Link
-                to="/perfil"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                <FaUser />
-              </Link>
+              {/* Menú de perfil en versión móvil */}
+              <div className="relative inline-block text-left">
+                <button
+                  onClick={handlePerfilClick}
+                  className="px-4 py-2 rounded-full transition duration-1000"
+                >
+                  <FaUser />
+                </button>
+                {perfilMenuVisible && (
+                  <div className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <div className="py-1">
+                      <Link
+                        to="/perfil"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Ir a Perfil
+                      </Link>
+                      <Link
+                        onClick={handlelogout}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Cerrar Sesión
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         )}
