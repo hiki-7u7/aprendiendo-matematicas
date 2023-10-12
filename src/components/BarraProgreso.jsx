a5039d05586a74fa1b78c79ea04dc34b3ceb44a9
DAPS2103 <daps2103@gmail.com>
2023-10-12 00:43:01 -0300

    creacion de componente barra de progreso

---
+++ src/components/BarraProgreso.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function BarraProgreso({ progress }) {
+  const progressPercentage = `${progress}%`;
+
+  return (
+    <div className="mt-2">
+      <div className="bg-gray-300 h-4 rounded-full">
+        <div
+          className="bg-blue-500 h-full rounded-full"
+          style={{ width: progressPercentage }}
+        ></div>
+      </div>
+      <p className="mt-1 text-gray-600">{progressPercentage}</p>
+    </div>
+  );
+}
