1fd2858caefcaf009450150e7b021c0296acd62b
DAPS2103 <daps2103@gmail.com>
2023-10-12 00:45:33 -0300

    modificacion pie de pagina

--- src/components/PieDePagina.jsx
+++ src/components/PieDePagina.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export function PieDePagina() {
   return (
-    <footer className="  bg-slate-300 text-black py-4 ">
-      <div className="container mx-auto text-center flex justify-center items-end h-full ">
-        <p>&copy; 2023 Diego Andres Peña Soto</p>
-      </div>
+    <footer className="  bg-slate-300 text-black py-4 relative bottom-0 ">
+      <div className="container mx-auto text-center justify-center items-end h-full w-10"></div>
     </footer>
   );
 }
