07b4af9af5261875be493be1606ddfe0c035ab0a
DAPS2103 <daps2103@gmail.com>
2023-10-19 12:09:17 -0300

    modificacion de alert utilizando toastify

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import { toast } from "react-toastify";
+
 export function Alert({ message }) {
   return (
     <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-2 text-center">
-      <span className="sm-:inline block">{message}</span>
+      <span className="sm-:inline block">
+        {toast.error(message, {
+          style: {
+            backgroundColor: "#FFCDD2", // verde mas claro #14B8A6 // verde mas oscuro #047857 // rojo #E53E3E
+            color: "#EF5350",
+          },
+          position: "top-center",
+          autoClose: 4000,
+          pauseOnHover: true,
+        })}
+      </span>
     </div>
   );
 }
