6de776d421d22c6c5aeb84e81fd2d5896cfa5aa4
DAPS2103 <daps2103@gmail.com>
2023-10-19 12:22:55 -0300

    modifcacion agregando colores al arxhivo tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue": "#0091FF",
+        "light-green": "##4BDD2A",
+        "dark-green": "#203432",
+      },
+    },
   },
   plugins: [],
 };
+
+// codigo para agregar el color #011626 en tailwind.config.js
+// theme: {
+//   extend: {
+//     colors: {
+//       "dark-blue": "#011626",
+//     },
+//   },
+
+//   // ...
+// },
